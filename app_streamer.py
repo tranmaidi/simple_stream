35cc5a25094afc8ecdf0ab759aa12fd759fb5e4a
tranmaidi <134763122+tranmaidi@users.noreply.github.com>
2025-04-23 11:23:09 +0700

    Update app_streamer.py

--- app_streamer.py
+++ app_streamer.py
@@ -5,27 +5,27 @@ RTC_CONFIGURATION = RTCConfiguration(
     {
       "iceServers": [
       {
-        urls: "stun:stun.relay.metered.ca:80",
+        "urls": "stun:stun.relay.metered.ca:80",
       },
       {
-        urls: "turn:global.relay.metered.ca:80",
-        username: "62a780ff22906dc4804b8101",
-        credential: "INzShUB/OF2KMxIJ",
+        "urls": "turn:global.relay.metered.ca:80",
+        "username": "62a780ff22906dc4804b8101",
+        "credential": "INzShUB/OF2KMxIJ",
       },
       {
-        urls: "turn:global.relay.metered.ca:80?transport=tcp",
-        username: "62a780ff22906dc4804b8101",
-        credential: "INzShUB/OF2KMxIJ",
+        "urls": "turn:global.relay.metered.ca:80?transport=tcp",
+        "username": "62a780ff22906dc4804b8101",
+        "credential": "INzShUB/OF2KMxIJ",
       },
       {
-        urls: "turn:global.relay.metered.ca:443",
-        username: "62a780ff22906dc4804b8101",
-        credential: "INzShUB/OF2KMxIJ",
+        "urls": "turn:global.relay.metered.ca:443",
+        "username": "62a780ff22906dc4804b8101",
+        "credential": "INzShUB/OF2KMxIJ",
       },
       {
-        urls: "turns:global.relay.metered.ca:443?transport=tcp",
-        username: "62a780ff22906dc4804b8101",
-        credential: "INzShUB/OF2KMxIJ",
+        "urls": "turns:global.relay.metered.ca:443?transport=tcp",
+        "username": "62a780ff22906dc4804b8101",
+        "credential": "INzShUB/OF2KMxIJ",
       },
       ]
     }
